d259619ad79e640540aaa63286ed7f9311835613
diazoxide <diazoxide@panteloma>
2019-05-01 13:00:04 +0400

    Creating plugin

---
+++ mdm.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: WP Mark Deleted Media
+Plugin URI: http://github.com/diazoxide/wp_mdm
+Description: This plugin finds deleted media before post save
+Version: 1.0
+Author: Aaron Yordanyan
+Author URI: https://www.linkedin.com/in/aaron-yor/
+License: GPL2
+*/
